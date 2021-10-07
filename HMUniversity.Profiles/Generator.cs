38688a893b8796ffc19e279a25ee24349e7b3511
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2021-10-07 12:22:20 +0100

    upf

--- HMUniversity.Profiles/Generator.cs
+++ HMUniversity.Profiles/Generator.cs
@@ -1,39 +1,43 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using HMUniversity.Profiles.Types;
 
 namespace HMUniversity.Profiles
 {
     public class Generator
     {
-        public const string OUTPUT_PATH = "outs";
+        public static string OutputPath { get; set; } = "outs";
 
         public static void Run(ProfileCollection pc)
         {
             var p = pc.Profiles;
-            foreach (var m in p)
-            {
-                Run(m);
-            }
+            var dic = p.ToDictionary(m => m.Name, Run);
         }
 
-        public static void Run(Profile p)
+        public static List<string> Run(Profile p)
         {
-            if (Directory.Exists(OUTPUT_PATH))
-                Directory.Delete(OUTPUT_PATH, true);
+            if (Directory.Exists(OutputPath))
+                Directory.Delete(OutputPath, true);
             var op = p.Operations;
+            var l = new List<string>();
             foreach (var o in op)
             {
                 if (o.Type == OperationType.Redirect)
                 {
                     foreach (var m in o.Source)
                     {
-                        string path = Path.Combine(OUTPUT_PATH, m);
+                        var path = Path.Combine(OutputPath, m);
                         Directory.CreateDirectory(path);
                         var n = CreateRedirectHtml(o.Target, null, 2);
                         File.WriteAllText(Path.Combine(path, "index.html"), n);
                     }
                 }
+
+                l.Add(o.Target);
             }
+
+            return l;
         }
 
         public static string CreateRedirectHtml(string target, string bodyHtml = null, int timeout = 0)
