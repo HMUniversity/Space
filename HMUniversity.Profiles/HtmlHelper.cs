09fbddd2d2743272f277823062f3929fe643f59a
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2021-10-07 12:48:19 +0100

    upd

--- HMUniversity.Profiles/HtmlHelper.cs
+++ HMUniversity.Profiles/HtmlHelper.cs
@@ -4,15 +4,35 @@
     {
         public static string CreateRedirectHtml(string target, string bodyHtml = null, int timeout = 0)
         {
-            bodyHtml ??= @"Redirecting to <a href=""" + target + @""">" + target + @"</a>...";
+            bodyHtml ??= $"Redirecting to {CreateTag("a", CreateProperty("href", target), target)}...";
 
-            return CreateHtml(@"<meta http-equiv=""refresh"" content=""" + timeout +
-                              @";url=" + target + @""">", bodyHtml);
+            return CreateHtml(
+                CreateFrontTag("meta",
+                    CreateProperty("http-equiv", "refresh") +
+                    CreateProperty("content", $"{timeout};url={target}")
+                ), bodyHtml);
         }
 
         public static string CreateHtml(string head, string body)
         {
-            return @"<html><head>" + head + "</head><body>" + body + "</body></html>";
+            return $"<html><head>{head}</head><body>{body}</body></html>";
+        }
+
+        public static string CreateFrontTag(string tag, string property = "", string value = "")
+        {
+            property = string.IsNullOrEmpty(property) ? string.Empty : " " + property;
+            return $"<{tag}{property}>{value}";
+        }
+
+        public static string CreateTag(string tag, string property = "", string value = "")
+        {
+            property = string.IsNullOrEmpty(property) ? string.Empty : " " + property;
+            return $"<{tag}{property}>{value}</{tag}>";
+        }
+
+        public static string CreateProperty(string key, string value)
+        {
+            return $"{key}=\"{value}\";";
         }
     }
 }
